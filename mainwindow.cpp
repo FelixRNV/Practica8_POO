510f653fae6516c359deb1d2f5e54c3f991b6a0b
Felix Navas <fnavas@est.ups.edu.ec>
2023-06-19 06:01:48 -0500

    Commit FileDialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFileDialog dialog;
+    dialog.setDirectory("/home/felix");
+    dialog.setWindowTitle("Selecionar Carpeta: ");
+    dialog.setFileMode(QFileDialog::Directory);
+    dialog.setOption(QFileDialog::ShowDirsOnly);
+
+    if (dialog.exec()) {
+            QStringList directoriosSeleccionados = dialog.selectedFiles();
+            foreach (QString directorio, directoriosSeleccionados) {
+                ui->txtDir->setText(directorio);
+            }
+        }
+
+}
+
