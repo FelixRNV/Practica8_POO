06521b25f268f23344b5928f7ee1f38f5400dde4
Felix Navas <fnavas@est.ups.edu.ec>
2023-06-19 05:15:35 -0500

    Commit Inicial

---
+++ analisisdir.cpp
@@ -0,0 +1,6 @@
+#include "analisisdir.h"
+
+AnalisisDir::AnalisisDir()
+{
+
+}
---
+++ analisisdir.h
@@ -0,0 +1,11 @@
+#ifndef ANALISISDIR_H
+#define ANALISISDIR_H
+
+
+class AnalisisDir
+{
+public:
+    AnalisisDir();
+};
+
+#endif // ANALISISDIR_H
